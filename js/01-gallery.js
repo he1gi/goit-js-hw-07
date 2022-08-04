c254d8c5ab2c9bd0043a0058a09308a73980aac1
he1gi <oleh.mishchenko1@gmail.com>
2022-08-04 23:34:06 +0300

    added final tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `  <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    })
+    .join("");
+}
+
+function onGalleryContainerClick(event) {
+  event.preventDefault();
+
+  const galleryImg = event.target.classList.contains("gallery__image");
+  if (!galleryImg) {
+    return;
+  }
+
+  const modal = basicLightbox.create(
+    `<img
+        class="gallery__image"
+        src="${event.target.dataset.source}"
+        alt="${event.target.description}"
+      />`,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", modalCloseHandler);
+      },
+      onClose: () => {
+        document.removeEventListener("keydown", modalCloseHandler);
+      },
+    }
+  );
+  modal.show();
+}
+
+function modalCloseHandler(event) {
+  if (event.code === "Escape") {
+    modal.close();
+  }
+}
